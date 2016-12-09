c8a91226e7874fe2a9dbac3dd2d3698fc966aa8f
cheshirecatnick <cheshirecatnick@gmail.com>
2016-12-09 14:58:39 +0800

    search scratch

--- js/search.js
+++ js/search.js
@@ -1,18 +1,35 @@
-
-function search() {
-  return shops.filter(filter);
-
-  ///
-   result = [];
-  for (let shop of shops) {
-    if (shop.?? <= query.?? && &&)
-      result.push(shop);
-  }
+'use strict';
+/**
+ * @description: search for shops that match query and sort by distance
+ * @author: Nicky
+*/
+let query = {
+  name: undefined,
+  wifi: true,
+  coffee: 4,
+  price: 5,
+  MRT: 1,
+  plug: true,
+  quiet: 3
+};
+function search(query) {
+  let result = shops
+    .filter(nameFilter)
+    .filter(wifiPlugFilter)
+    .filter(coffeePriceQuietFilter);
+  // sort result by distance and MRT
   return result;
 }
 
+function nameFilter(shop) {
 
-function filter() {
+
+};
+function wifiPlugFilter(shop) {
+
+
+};
+function coffeePriceQuietFilter(shop) {
 
 
 };
