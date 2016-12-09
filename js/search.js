98adfe87ad437e0d7bb5825afaa9280198846ec7
cheshirecatnick <cheshirecatnick@gmail.com>
2016-12-09 14:32:12 +0800

    add search.js

---
+++ js/search.js
@@ -0,0 +1,18 @@
+
+function search() {
+  return shops.filter(filter);
+
+  ///
+   result = [];
+  for (let shop of shops) {
+    if (shop.?? <= query.?? && &&)
+      result.push(shop);
+  }
+  return result;
+}
+
+
+function filter() {
+
+
+};
